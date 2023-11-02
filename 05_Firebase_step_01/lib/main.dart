1792b77526ac2857640d5d28c0e880c3120163c2
alanwnl <wongalan2002@yahoo.com.hk>
2023-11-02 17:27:38 +0800

    05 step 01

--- 05_Firebase_step_01/lib/main.dart
+++ 05_Firebase_step_01/lib/main.dart
@@ -1,13 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:my_app/questions_screen.dart';
 import 'package:my_app/topics_screen.dart';
 import 'package:my_app/tts_play_state.dart';
 import 'package:provider/provider.dart';
 
 import 'appstate.dart';
-import 'mcq_screen.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     MultiProvider(
       providers: [
